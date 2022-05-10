8e5ec67bcd4ab1351687bbc9e9cd8c9c9da5a9e8
Tele2 Russia <tele2.playmarket@gmail.com>
2022-05-10 13:32:13 +0300

    1 version

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Signer } from '@waves/signer';
+import { ProviderWeb } from '@waves.exchange/provider-web';
+
+const signer = new Signer( {
+  // Specify URL of the node on Testnet
+  NODE_URL: 'https://nodes-testnet.wavesnodes.com'
+});
+signer.setProvider(new ProviderWeb());
+
+function sendData() {
+    console.log("sending data");
+    const name = document.querySelector(".data").value;
+    signer.data({data: [{type: "string", key: name, value: "verified"}]}).broadcast();
+}
+
+function init() {
+    document.querySelector(".add-button").addEventListener("click", sendData);
+}
+
+document.addEventListener("DOMContentLoaded", init);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const main = (name, minimize) => ({
+    entry: './src/index.ts',
+    mode: "production",
+    optimization: {
+        minimize,
+        usedExports: true
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        globalObject: "dApp",
+        filename: name,
+        path: path.resolve(__dirname, 'dist'),
+    }
+});
+
+module.exports = [
+    {
+        ...main('dApp.js', false),
+        devtool: 'inline-source-map',
+        mode: "production",
+    },
+    {
+        ...main('dApp.min.js', true)
+    }
+];
